181b8de3725199e5e6092621d9bc319aea69df05
obada <obadaquraan@yahoo.com>
2020-01-23 22:45:42 +0200

    add test and .eslint

---
+++ arrayBinarySearch.test.js
@@ -0,0 +1,14 @@
+'use strict' ;
+
+const binarySearch=require('./arrayBinarySearch/array-binary-search.js')
+
+describe('test binarySearch ',()=>{
+    it('get the right index',()=>{
+
+        let num =2
+        let arr=[1,2,3]
+        
+        expect(binarySearch(arr,num)).toEqual(1)
+
+    })
+})
--- arrayBinarySearch/array-binary-search.js
+++ arrayBinarySearch/array-binary-search.js
@@ -20,3 +20,7 @@ function binarySearch(arr, num) {
     }
     return j
 }
+
+
+
+module.exports=binarySearch;
