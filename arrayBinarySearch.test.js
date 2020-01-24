a0d28186c336eb4d684f03e1a760bfede85416f5
obada <obadaquraan@yahoo.com>
2020-01-25 00:10:52 +0200

    eslinrc

--- arrayBinarySearch.test.js
+++ arrayBinarySearch.test.js
@@ -5,10 +5,10 @@ const binarySearch=require('./arrayBinarySearch/array-binary-search.js')
 describe('test binarySearch ',()=>{
     it('get the right index',()=>{
 
-        let num =2
-        let arr=[1,2,3]
+        let num = 2
+        let arr = [1,2,3]
         
-        expect(binarySearch(arr,num)).toEqual(1)
+expect(binarySearch(arr,num)).toEqual(1)
 
     })
 })
--- arrayBinarySearch/array-binary-search.js
+++ arrayBinarySearch/array-binary-search.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// Input	
-// [4,8,15,16,23,42], 15	
-
-// Output
-// 2
-
-// Input
-// [11,22,33,44,55,66,77], 90
-// Output
-// -1
-
-
 function binarySearch(arr, num) {
     var j
     for (let i = 0; i < arr.length; i++) {
