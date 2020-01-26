f2464d7057d35d9ac1084cb61f39dda54a255037
obada <obadaquraan@yahoo.com>
2020-01-26 22:33:36 +0200

    fix

--- linkedlist/linked.js
+++ linkedlist/linked.js
@@ -1,54 +1,70 @@
 'use strict';
 
-const {LinkedList,Node}=require('./linkedlist.js')
+const {LinkedList,Node,} = require('./linkedlist.js');
 
 class ll extends LinkedList {
-    constructor(){
-        super()
+  constructor(){
+    super();
+  }
+
+  append(value) {
+    let node = new Node(value);
+
+    // { value: 5, next: null }
+    
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+    let currentNode = this.head;
+    while(currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+  }
+
+  insertBefore(value, newVal){
+
+    let node = new Node(newVal);
+    let current = this.head;
+    let previews = this.head;
+
+    while(current){
+      // let indx=0
+      // indx+=1
+      if(current.value === value){
+        //    if(indx===1){current=node;console.log('hi1') ;return this}
+        previews.next = node;
+        node.next = current;
+      }
+      else{previews = current;}
+      current = current.next;
+
     }
 
-    append(value) {
-        let node = new Node(value);
-    
-        // { value: 5, next: null }
-        console.log('this.head : ', this.head);
-        if (!this.head) {
-          this.head = node;
-          return this;
-        }
-        let currentNode = this.head;
-        while(currentNode.next) {
-          currentNode = currentNode.next
-        }
-        currentNode.next = node;
-        return this;
+  }
+
+  insertAfter(value, newVal){
+    let node = new Node(newVal);
+    let current = this.head;
+
+    while(current){
+      if(current.value === value){
+        node.next = current.next;
+        current.next = node;
+        break;
       }
 
-      insertBefore(value, newVal){
+      current = current.next;
+    }
+  }
 
-        let node = new Node(newVal);
-        let current=this.head;
-        let previews=this.head;
 
-            while(current){
-            
-                if(current.value===value){
-                    previews.next=node
-                    node.next=current
-                }
-                else{previews=current}
-                current=current.next;
-            }
-           
-          }
-
-      
-      
 }
-let ls= new ll;
-console.log('ls.apped() : ', ls.append(1));
-console.log('ls.apped() : ', ls.append(2));
-console.log('ls.apped() : ', ls.append(3));
-console.log('ls.insertBefore(3,10) : ', ls.insertBefore(2,10));
+let ls = new ll;
 
-console.log('ls : ', ls.printList());
+
+
+
+module.exports = ll;
--- linkedlist/linkedlist.js
+++ linkedlist/linkedlist.js
@@ -1,12 +1,12 @@
 'use strict';
 
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
-
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
+
+}
 
 class LinkedList {
   constructor() {
@@ -17,15 +17,14 @@ class LinkedList {
     let node = new Node(value);
 
     // { value: 5, next: null }
-    console.log('this.head : ', this.head);
     if (!this.head) {
       this.head = node;
       return this;
     }
-    
+
     let currentNode = this.head;
     while(currentNode.next) {
-      currentNode = currentNode.next
+      currentNode = currentNode.next;
     }
 
     currentNode.next = node;
@@ -34,35 +33,27 @@ class LinkedList {
 
   include(val){
 
-    let current=this.head;
+    let current = this.head;
     while(current){
-        if(current.value===val){
-            return true;
-        }
-        current=current.next;
+      if(current.value === val){
+        return true;
+      }
+      current = current.next;
     }
     return false;
   }
 
-  printList() 
-{ 
-    let current = this.head; 
-    let str = "{"; 
-    while (current) { 
-        str += current.value + "} -> {"; 
-        current = current.next; 
-    } 
-    str += " NULL }"; 
-    // console.log(str); 
+  printList()
+  {
+    let current = this.head;
+    let str = '{';
+    while (current) {
+      str += current.value + '} -> {';
+      current = current.next;
+    }
+    str += ' NULL }';
     return str;
-} 
+  }
 }
-let ll= new LinkedList();
-// console.log('insert(5) : ',ll.insert(5));
-// console.log('insert(5) : ',ll.insert(10));
-// console.log('insert(5) : ',ll.insert(15));
-
-// console.log('print : ',ll.printList());
-// console.log('ll.head : ', ll.head);
 
 module.exports = {LinkedList,Node};
