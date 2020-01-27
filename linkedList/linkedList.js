28254ed8f7ff61e85384dbd3921a9c30f056f63f
obada <obadaquraan@yahoo.com>
2020-01-27 22:36:55 +0200

    lint

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -1,111 +1,111 @@
 'use strict';
 
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    let node = new Node(value);
+
+    // { value: 5, next: null }
+    if (!this.head) {
+      this.head = node;
+      return this;
     }
 
-    insert(value) {
-        let node = new Node(value);
-
-        // { value: 5, next: null }
-        if (!this.head) {
-            this.head = node;
-            return this;
-        }
-
-        let currentNode = this.head;
-        while (currentNode.next) {
-            currentNode = currentNode.next;
-        }
-
-        currentNode.next = node;
-        return this;
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
     }
 
-    include(val) {
+    currentNode.next = node;
+    return this;
+  }
 
-        let current = this.head;
-        while (current) {
-            if (current.value === val) {
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
+  include(val) {
+
+    let current = this.head;
+    while (current) {
+      if (current.value === val) {
+        return true;
+      }
+      current = current.next;
     }
+    return false;
+  }
 
-    printList() {
-        let current = this.head;
-        let str = '{';
-        while (current) {
-            str += current.value + '} -> {';
-            current = current.next;
-        }
-        str += ' NULL }';
-        return str;
+  printList() {
+    let current = this.head;
+    let str = '{';
+    while (current) {
+      str += current.value + '} -> {';
+      current = current.next;
     }
-    append(value) {
-        let node = new Node(value);
+    str += ' NULL }';
+    return str;
+  }
+  append(value) {
+    let node = new Node(value);
 
-        // { value: 5, next: null }
+    // { value: 5, next: null }
 
-        if (!this.head) {
-            this.head = node;
-            return this;
-        }
-        let currentNode = this.head;
-        while (currentNode.next) {
-            currentNode = currentNode.next;
-        }
-        currentNode.next = node;
-        return this;
+    if (!this.head) {
+      this.head = node;
+      return this;
     }
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+  }
 
-    insertBefore(value, newVal) {
+  insertBefore(value, newVal) {
 
-        let node = new Node(newVal);
-        let current = this.head;
-        let previews = this.head;
+    let node = new Node(newVal);
+    let current = this.head;
+    let previews = this.head;
 
-        while (current) {
-            // let indx=0
-            // indx+=1
-            if (current.value === value) {
-                //    if(indx===1){current=node;console.log('hi1') ;return this}
-                previews.next = node;
-                node.next = current;
-            }
-            else { previews = current; }
-            current = current.next;
-
-        }
+    while (current) {
+      // let indx=0
+      // indx+=1
+      if (current.value === value) {
+        //    if(indx===1){current=node;console.log('hi1') ;return this}
+        previews.next = node;
+        node.next = current;
+      }
+      else { previews = current; }
+      current = current.next;
 
     }
 
-    insertAfter(value, newVal) {
-        let node = new Node(newVal);
-        let current = this.head;
+  }
 
-        while (current) {
-            if (current.value === value) {
-                node.next = current.next;
-                current.next = node;
-                break;
-            }
+  insertAfter(value, newVal) {
+    let node = new Node(newVal);
+    let current = this.head;
 
-            current = current.next;
-        }
+    while (current) {
+      if (current.value === value) {
+        node.next = current.next;
+        current.next = node;
+        break;
+      }
+
+      current = current.next;
     }
+  }
 
 }
 
-module.exports = { LinkedList, Node };
+module.exports = { LinkedList, Node};
