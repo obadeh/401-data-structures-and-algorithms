33a1edda5139b28be0d370e61cf185f042eea5ea
obada <obadaquraan@yahoo.com>
2020-01-27 23:08:21 +0200

    fix github actions

--- arrayBinarySearch/array-binary-search.js
+++ arrayBinarySearch/array-binary-search.js
@@ -1,14 +1,14 @@
-'use strict';
+
 
 function binarySearch(arr, num) {
-    var j
-    for (let i = 0; i < arr.length; i++) {
-        j = -1;
-        if (arr[i] === num) { return j = i }
-    }
-    return j
+  var j;
+  for (let i = 0; i < arr.length; i++) {
+    j = -1;
+    if (arr[i] === num) { return j = i; }
+  }
+  return j;
 }
 
 
 
-module.exports=binarySearch;
+module.exports = binarySearch;
