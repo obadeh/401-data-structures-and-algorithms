a0c1254e52b9dcec5d4bb0a5aa8199da39a1c879
obada <obadaquraan@yahoo.com>
2020-02-03 21:48:26 +0200

    ss

--- README.md
+++ README.md
@@ -2,17 +2,18 @@
 
 # Challenge Summary
 
-Create a brand new PseudoQueue class. methods : enqueue , dequeue
+Create a class called AnimalShelter which holds only dogs and cats.
 
 ## Challenge Description
 
-Create a brand new PseudoQueue class.  PseudoQueue class will implement our standard queue interface (the two methods listed below), but will internally only utilize 2 Stack objects. Ensure that you create your class with the following methods:
-enqueue which inserts value into the PseudoQueue, using a first-in, first-out approach.
-dequeue which extracts a value from the PseudoQueue, using a first-in, first-out approach.
+The shelter operates using a first-in, first-out approach.
+Implement the following methods:
+enqueue(animal): adds animal to the shelter. animal can be either a dog or a cat object.
+dequeue(pref): returns either a dog or a cat. If pref is not "dog" or "cat" then return null.
 
 ## Approach & Efficiency
 it taked 2 hours 
 
 ## Solution
 
-![wb](./ii.jpj)
+
---
+++ fifoAnimalShelter/fifo-animal-shelter.js
@@ -0,0 +1,57 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  
+  }
+
+
+class AnimalShelter{
+    constructor(){
+        this.head=null;
+    }
+
+
+    enqueue(value){
+        let node
+        if(value ==='dog' || value ==='cat'){ node  =new Node(value)}
+        else{return null}
+       
+        let current= this.head;
+        if(!this.head){
+            this.head=node;
+            return this;
+        }
+        while(current.next) {
+            current = current.next;
+          }
+      
+          current.next = node;
+          return this;
+    }
+
+    dequeue(value){
+        let node;
+        if(value ==='dog' || value ==='cat'){ node  =new Node(value)}
+        else{return null}
+
+        if(!this.head){
+            return null;
+        }
+        this.head = this.head.next;
+        return this.head;
+    }
+}
+
+
+let a= new AnimalShelter()
+
+a.enqueue('dog')
+a.enqueue('cat')
+
+console.log('a : ', a);
+a.dequeue('cat')
+console.log('a : ', a);
+
+module.exports=AnimalShelter
