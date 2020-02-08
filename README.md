5da293a52e3c4783c32d91c8acaed7c16f1e32e4
obada <obadaquraan@yahoo.com>
2020-02-08 20:58:13 +0200

    dd

--- README.md
+++ README.md
@@ -2,15 +2,10 @@
 
 # Challenge Summary
 
- function should take a string as its only argument, and should return a boolean representing whether or not the brackets in the string are balanced. 
 
 ## Challenge Description
 
-here are 3 types of brackets:
 
-Round Brackets : () ==> true
-Square Brackets : [] ==> true
-Curly Brackets : {} ==> true
 
 
 ## Approach & Efficiency
@@ -18,4 +13,4 @@ it taked 2 hours
 
 ## Solution
 
-![whbord](./imgk.jpg)
+![whbord]()
--- __test__/multiBracketValidator.test.js
+++ __test__/multiBracketValidator.test.js
@@ -1,20 +1,2 @@
 'use strict';
 
-const validator= require('../multiBracketValidation/multi-bracket-validation.js')
-
-describe('test the multi bracket validation function', () => {
-  test('check for balance brackets strings that should return true', () => {
-    expect(validator('()')).toEqual(true);
-    expect(validator('({})')).toEqual(true);
-    expect(validator('[()()]')).toEqual(true);
-  });
-
-  test('check strings that should return false', () => {
-    expect(validator('([)')).toEqual(false);
-    expect(validator(')')).toEqual(false);
-    expect(validator('[})')).toEqual(false);
-    expect(validator('}')).toEqual(false);
-    expect(validator('(')).toEqual(false);
-
-  });
-});
