713d6544f58f8460a5a386abba1ea26ca9689124
obada <obadaquraan@yahoo.com>
2020-02-11 07:46:11 +0200

    first part

--- tree/tree.js
+++ tree/tree.js
@@ -1,85 +1,56 @@
 "use strict";
 
-class BinarySearchTree {
-  constructor() {
-    //initially root is null
-
-    this.root = null;
-  }
-
-  insertNumberNode(data, left = null, right = null) {
-    //creating a Node
-    //data we pass will act as individual parent Node
-    //left and right are subtrees
-    let Node = {
-      data,
-      left,
-      right
-    };
-    //suppose currentNumberNode as a parent node
-    //current Num Node value decides position of next value
-    //if it goes to left subtree or right subtree
-    let currentNumberNode;
-
-    if (!this.root) {
-      //if its not a root make it one by passing a Node
-      this.root = Node;
-    } else {
-      //and if its a root now, assign it to currentNumberNode
-      currentNumberNode = this.root;
-      while (currentNumberNode) {
-        //if data is smaller than cuurent data, send it in left subtree
-        if (data < currentNumberNode.data) {
-          //if current num node don't have Node properties
-          //we will assign it node properties
-          if (!currentNumberNode.left) {
-            currentNumberNode.left = Node;
-            break;
-          } else {
-            //if it has node properties and it is sent by root to left
-            //we will make it a left node because it is smaller than root node
-            currentNumberNode = currentNumberNode.left;
-          }
-          //if data is larger than cuurent data, send it in right subtree
-        } else if (data > currentNumberNode.data) {
-          //if current num node don't have Node properties
-          //we will assign it node properties
-          if (!currentNumberNode.right) {
-            currentNumberNode.right = Node;
-            break;
-          } else {
-            //if it has node properties and it is sent by root to right
-            //we will make it a right node because it is larger than root node
-            currentNumberNode = currentNumberNode.right;
-          }
-        } else {
-          console.log("Try Different Value");
-          break;
-        }
-      }
-    }
+class Node{
+  constructor(value, left = null, right = null) {
+    this.value = value; // passed in
+    this.left = left; // null
+    this.right = right; // null
   }
 }
-let BSTtest = new BinarySearchTree();
 
-//tests
 
-BSTtest.insertNumberNode(10);
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
 
-BSTtest.insertNumberNode(7);
+  // Data -> read the node, Left -> go left, Right -> go right
+  // DLR
+  preOrder() {
+    let results = [];
 
-BSTtest.insertNumberNode(14);
+    let _walk = (node) => {
+      results.push(node.value);
+      if (node.left) _walk(node.left);
+      if (node.right) _walk(node.right);
+    }
+    _walk(this.root);
+    return results;
+  }
+  // LDR
+  inOrder() {
+    let results = [];
 
-BSTtest.insertNumberNode(5);
+    let _walk = (node) => {
+      if (node.left) _walk(node.left);
+      results.push(node.value);
+      if (node.right) _walk(node.right);
+    }
+    _walk(this.root);
+    return results;
+  }
 
-BSTtest.insertNumberNode(13);
+  postOrder(){
+    let results = [];
 
-BSTtest.insertNumberNode(8);
+    let _walk = (node) => {
+      if (node.left) _walk(node.left);
+      if (node.right) _walk(node.right);
+      results.push(node.value);
+    }
+    _walk(this.root);
+    return results;
+  }
+}
 
-BSTtest.insertNumberNode(18);
-
-BSTtest.insertNumberNode(15);
-
-BSTtest.insertNumberNode(6);
-
-console.log('BSTtest : ', BSTtest);
+module.exports = {Node,BinaryTree};
