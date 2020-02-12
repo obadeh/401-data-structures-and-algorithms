1870fc15a181d98c145422a52d9bf8b5d75b14e4
obada <obadaquraan@yahoo.com>
2020-02-12 21:37:13 +0200

    fixed

---
+++ __test__/queueWithStacks.test.js
@@ -0,0 +1,16 @@
+const PseudoQueue = require('../queueWithStacks/queue-with-stacks')
+
+const queue = new PseudoQueue();
+
+describe('Using stacks to imitate a queue', () => {
+  test('enqueue(value) which inserts , using a first-in, first-out approach.', () => {
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    expect(queue.stack1).toEqual([3, 2, 1]);
+  });
+  test('dequeue extracts a value , using a first-in, first-out approach.', () => {
+    queue.dequeue();
+    expect(queue.stack1).toEqual([3, 2]);
+  });
+});
