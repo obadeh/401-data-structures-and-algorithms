af132fdb9cc8ab6b2678bd8d937f557fac1910f8
obada <obadaquraan@yahoo.com>
2020-02-12 20:38:30 +0200

    bst

--- tree/tree.js
+++ tree/tree.js
@@ -1,6 +1,6 @@
 "use strict";
 
-class Node{
+class Node {
   constructor(value, left = null, right = null) {
     this.value = value; // passed in
     this.left = left; // null
@@ -40,7 +40,7 @@ class BinaryTree {
     return results;
   }
 
-  postOrder(){
+  postOrder() {
     let results = [];
 
     let _walk = (node) => {
@@ -53,4 +53,87 @@ class BinaryTree {
   }
 }
 
-module.exports = {Node,BinaryTree};
+class BinarySearchTree {
+  constructor() {
+    // root of a binary seach tree 
+    this.root = null;
+  }
+
+
+  insert(data) {
+    // Creating a node and initailising  
+    // with data  
+    var newNode = new Node(data);
+
+    // root is null then node will 
+    // be added to the tree and made root. 
+    if (this.root === null)
+      this.root = newNode;
+    else
+
+      // find the correct position in the  
+      // tree and add the node 
+      this.insertNode(this.root, newNode);
+  }
+
+  // Method to insert a node in a tree 
+  // it moves over the tree to find the location 
+  // to insert a node with a given data  
+  insertNode(node, newNode) {
+    // if the data is less than the node 
+    // data move left of the tree  
+    if (newNode.data < node.data) {
+      // if left is null insert node here 
+      if (node.left === null)
+        node.left = newNode;
+      else
+
+        // if left is not null recurr until  
+        // null is found 
+        this.insertNode(node.left, newNode);
+    }
+
+    // if the data is more than the node 
+    // data move right of the tree  
+    else {
+      // if right is null insert node here 
+      if (node.right === null)
+        node.right = newNode;
+      else
+
+        // if right is not null recurr until  
+        // null is found 
+        this.insertNode(node.right, newNode);
+    }
+  }
+
+  contain(value) {
+    let found = false,
+      current = this._root
+
+    //make sure there's a node to search
+    while (!found && current) {
+
+      //if the value is less than the current node's, go left
+      if (value < current.value) {
+        current = current.left;
+
+        //if the value is greater than the current node's, go right
+      } else if (value > current.value) {
+        current = current.right;
+
+        //values are equal, found it!
+      } else {
+        found = true;
+      }
+    }
+
+    //only proceed if the node was found
+    return found;
+  }
+
+
+}
+
+
+module.exports = { Node, BinaryTree };
