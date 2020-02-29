3afaf0de63cbe52d898b373545096f2c2a505aec
obada <obadaquraan@yahoo.com>
2020-03-01 00:10:05 +0200

    fixing

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 # 401-data-structures-and-algorithms
 
 # Challenge Summary
-Write an instance method called find-maximum-value. Without utilizing any of the built-in methods available to your language, return the maximum value stored in the tree. You can assume that the values stored in the Binary Tree will be numeric.
+Implement a Hashtable with the following methods:
+
+* add: takes in both the key and value. This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
+* get: takes in the key and returns the value from the table.
+* contains: takes in the key and returns a boolean, indicating if the key exists in the table already.
+* hash: takes in an arbitrary key and returns an index in the collection.
 
 
 ## Approach & Efficiency
@@ -10,4 +15,4 @@ it taked 2 hours
 
 ## Solution
 
-![whbord](./mbt.jpg)
+![whbord](./llk.jpg)
---
+++ hashtable/hashtable.js
@@ -0,0 +1,114 @@
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(value) {
+    if(!value) throw new Error('Invalid value');
+
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+
+    while(current.next) {
+      current = current.next;
+    }
+
+    current.next = node;
+  }
+
+  getWithKey(key) {
+    if (!this.head) return;
+
+    let current = this.head;
+
+    while (current) {
+      if (current.value[0] === key) return current.value[1];
+      current = current.next;
+    }
+
+    return;
+  }
+
+  values() {
+    let values = [];
+    let current = this.head;
+
+    while(current) {
+      values.push(current.value);
+      current = current.next;
+    }
+
+    return values;
+  }
+}
+
+class HashTable {
+  constructor(size) {
+    this.size = size || 5,
+    this.buckets = new Array(this.size);
+  }
+
+  hash(key) {
+    // This is the actual hash function controling where the values get placed
+    let hash = key.split('').reduce((prev, curr) => prev + curr.charCodeAt(0), 0) * 599 % this.size;
+
+    return hash;
+  }
+
+  add(key, value) {
+    if (!key) throw new Error('Invalid key provided');
+    if (this.contains(key)) return 'Key already being used';
+
+    let index = this.hash(key);
+
+    if (!this.buckets[index])  { this.buckets[index] = new LinkedList(); }
+
+    try{
+      // this.buckets is an array
+      // this.buckets[index] is a Linked List
+      // Must put key and value for lookup
+      this.buckets[index].add([key, value]);
+    } catch(err) {
+      throw err;
+    }
+  }
+
+  get(key) {
+    if (!key) throw new Error('Invalid key provided');
+
+    let index = this.hash(key);
+
+    if (!this.buckets[index])  { return null; }
+
+    console.log(this.buckets);
+
+    return this.buckets[index].getWithKey(key);
+  }
+
+  contains(key) {
+    if (!key) throw new Error('Invalid key provided');
+
+    let index = this.hash(key);
+
+    if (!this.buckets[index])  { return null; }
+
+    return this.buckets[index].getWithKey(key) ? true : false;
+  }
+}
+
+module.exports = { HashTable, LinkedList };
